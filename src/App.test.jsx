4be4a0196005c9d50f026bb0501010a074ee7869
Kevin Do <kevind0501@gmail.com>
2022-01-24 14:31:22 -0800

    passing app.test.js tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,15 @@
+import { screen, render } from '@testing-library/react'
+import App from './App'
 
-test('Should render the header', () => {
+test('Should render the header', async () => {
+  render(<App />)
 
+  const image = screen.getByAltText(/alchemy/i)
+  expect(image).toBeInTheDocument()
+
+  const heading = screen.getByText(/Meet/i)
+  expect(heading).toBeInTheDocument()
+
+  const name = await screen.findByText(/Vonta/i)
+  expect(name).toBeInTheDocument()
 })
