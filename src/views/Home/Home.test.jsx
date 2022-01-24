35e8a44aef5d4c26227455ac9d585b3c90593fb9
Kevin Do <kevind0501@gmail.com>
2022-01-24 14:53:12 -0800

    passing home tests

--- src/views/Home/Home.test.jsx
+++ src/views/Home/Home.test.jsx
@@ -1,3 +1,5 @@
+import { screen, render, waitForElementToBeRemoved } from '@testing-library/react'
+import Home from './Home'
 
 const user = {
   id: 1,
@@ -10,6 +12,29 @@ const user = {
   color: 'crimson',
 }
 
-test('Should render the user profile', () => {
+test('Should render the user profile', async () => {
+  render(<Home user={user} />)
 
+  const banner = screen.getByAltText(/header/i)
+  expect(banner).toBeInTheDocument()
+
+  const avatar = screen.getByAltText(/avatar/i)
+  expect(avatar).toBeInTheDocument()
+
+  const name = await screen.findByText(/Vonta/i)
+  expect(name).toBeInTheDocument()
+
+  const motto = await screen.findByText(/res non verba/i)
+  expect(motto).toBeInTheDocument()
+
+  const color = await screen.findByText(/favorite/i)
+  expect(color).toBeInTheDocument()
+
+  const interests = await screen.findByText(/interests/i)
+  expect(interests).toBeInTheDocument()
+
+  const interestsList = await screen.findByText(/living/i)
+  expect(interestsList).toBeInTheDocument()
+
+  // await waitForElementToBeRemoved(() => screen.getByText(/loading/i))
 })
